d202474c20a2abb4e3162125336436e4c8c42e5c
Debanjan Chakraborty <frontlook.inc@gmail.com>
2019-12-30 11:10:19 +0530

    Add project files.

---
+++ frontlook_dotnetframework_library/FL_webpage/FL_general/FL_response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontlook_dotnetframework_library.FL_webpage.FL_general
+{
+    public class FL_response
+    {
+        public static string FL_message(string message)
+        {
+            return "<script language='javascript'>" +
+                   "window.alert('Salary Head Is Already Present With Name " +
+                   message + "');" +
+                   "</script>";
+        }
+
+        public static string FL_message_and_redirect(string message)
+        {
+            return "<script language='javascript'>" +
+                   "window.alert('Salary Head Is Already Present With Name " +
+                   message + "');" +
+                   "</script>";
+        }
+    }
+}
